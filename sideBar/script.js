0457662faa2d7dcf63b83e4f0475cbabe64e923e
Muhammad Umair <muhammadumair7211@gmail.com>
2025-03-17 01:29:47 +0500

    Delete sideBar directory

--- sideBar/script.js
+++
@@ -1,9 +0,0 @@
-let sideBar = document.querySelector(".side-bar");
-let closeButton = document.querySelector(".close");
-let openBar = document.querySelector(".bars");
-openBar.addEventListener("click", () => {
-	sideBar.classList.toggle("show-sidebar");
-});
-closeButton.addEventListener("click", () => {
-	sideBar.classList.remove("show-sidebar");
-});
